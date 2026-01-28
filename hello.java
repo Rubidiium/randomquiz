e60ec279c7f423c27e51375ba3f0ee0037970d65
DarkestGolf8600 <ramirj6@rpi.edu>
2026-01-28 13:00:04 -0500

    Adding print statements

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
 public class hello {
     public static void main(String args[]){
         System.out.println("Hello!");
+        System.out.println("HELLO FOR THIS PART OF QUIZ!!!");
+        System.out.println("2310");
     }
 }
